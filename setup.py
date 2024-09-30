83229585b8287ed814345dcbe82b2b69d17a7a7f
Haiming <zhuhaiming921214@gmail.com>
2024-09-30 23:18:32 +0800

    change version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
    name='vivsim',
    packages=setuptools.find_packages(),
-   version='1.0',
+   version='v0.2.0',
    description='Accelerated vortex-induced vibration (VIV) simulation using immersed boundary lattice Boltzmann method (IB-LBM) powered by JAX.',
    author='Haiming Zhu',
    author_email='zhuhaiming@tju.edu.cn',
