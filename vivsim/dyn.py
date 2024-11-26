a7359067b46700c159b85ebdffd4fd9c01e5d309
haimingz <zhuhaiming921214@gmail.com>
2024-11-27 02:25:55 +0800

    improve docstring

--- vivsim/dyn.py
+++ vivsim/dyn.py
@@ -1,9 +1,27 @@
 import jax.numpy as jnp
 
-def newmark2dof(a, v, d, h, m, k, c, dt=1, gamma=0.5, beta=0.25):
-    """Newmark-beta method for dynamics"""
+def newmark_2dof(a, v, d, h, m, k, c, dt=1, gamma=0.5, beta=0.25):
+    """Newmark-beta method for dynamics
+    
+    Args: 
+        a (float): acceleration at time t
+        v (float): velocity at time t
+        d (float): displacement at time t
+        h (float): external force at time t
+        m (float): mass
+        k (float): stiffness
+        c (float): damping
+        dt (float): time step
+        gamma (float): gamma parameter for Newmark-beta method
+        beta (float): beta parameter for Newmark-beta method
+    
+    Returns:
+        a_next (float): acceleration at time t+1
+        v_next (float): velocity at time t+1
+        d_next (float): displacement at time t+1
+    """
 
-    c1, c2 = gamma * dt, beta * dt**2
+    c1, c2 = gamma * dt, beta * dt ** 2
     v1 = v + dt * (1 - gamma) * a
     v2 = d + dt * v + dt**2 * (0.5 - beta) * a
     a_next = (h - c * v1 - k * v2) / (m + c1 * c + c2 * k)
@@ -12,8 +30,26 @@ def newmark2dof(a, v, d, h, m, k, c, dt=1, gamma=0.5, beta=0.25):
     return a_next, v_next, d_next
 
 
-def newmark3dof(a, v, d, h, m, k, c, dt=1, gamma=0.5, beta=0.25):
-    """Newmark-beta method for dynamics"""
+def newmark_3dof(a, v, d, h, m, k, c, dt=1, gamma=0.5, beta=0.25):
+    """Newmark-beta method for dynamics
+    
+    Args:
+        a (matrix): acceleration at time t
+        v (matrix): velocity at time t
+        d (matrix): displacement at time t
+        h (matrix): external force at time t
+        m (matrix): mass
+        k (matrix): stiffness
+        c (matrix): damping
+        dt (matrix): time step
+        gamma (matrix): gamma parameter for Newmark-beta method
+        beta (matrix): beta parameter for Newmark-beta method
+    
+    Returns:
+        a_next (matrix): acceleration at time t+1
+        v_next (matrix): velocity at time t+1
+        d_next (matrix): displacement at time t+1
+    """
 
     c1, c2 = gamma * dt, beta * dt ** 2
 
