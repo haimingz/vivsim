3994a1d2afcd430ab527d19abf6f63b7f5458028
haimingz <zhuhaiming921214@gmail.com>
2024-12-04 22:32:48 +0800

    turns out the XLA_FLAGS should be set before importing jax

--- examples/vortex_induced_vibration.py
+++ examples/vortex_induced_vibration.py
@@ -4,6 +4,15 @@
 # domain and is free to move in the flow. The flow is driven by a constant
 # velocity U0 in the x direction. 
 
+import os
+os.environ['XLA_FLAGS'] = (
+    '--xla_gpu_enable_triton_softmax_fusion=true '
+    '--xla_gpu_triton_gemm_any=True '
+    # '--xla_gpu_enable_async_collectives=true '
+    '--xla_gpu_enable_latency_hiding_scheduler=true '
+    '--xla_gpu_enable_highest_priority_async_stream=true '
+)
+
 import math
 import jax
 import jax.numpy as jnp
