02f913dd1f11a3c6e969f31cb14a081df79bdc9e
haimingz <zhuhaiming921214@gmail.com>
2024-12-17 02:16:02 +0800

    optimize streaming, collision, and forcing functions in the way that favors parallelism to improve efficiency

--- vivsim/lbm.py
+++ vivsim/lbm.py
@@ -38,28 +38,48 @@ Note:
       so that they can be modified in-place to avoid unnecessary memory allocation/deallocation. 
 """
 
-import jax
 import jax.numpy as jnp
+
+WEIGHTS = jnp.array([4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36])
+
+VELOCITIES = jnp.array([
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+    [1, 1],
+    [-1, 1],
+    [-1, -1],
+    [1, -1]
+])
+
+RIGHT_DIRS = jnp.array([1, 5, 8])
+LEFT_DIRS = jnp.array([3, 7, 6])
+UP_DIRS = jnp.array([2, 5, 6])
+DOWN_DIRS = jnp.array([4, 7, 8])
+ALL_DIRS = jnp.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+OPP_DIRS = jnp.array([0, 3, 4, 1, 2, 7, 8, 5, 6])
+
 
 def streaming(f):
     """Perform the streaming step by shifting the distribution functions 
-    along their respective lattice directions, which automatically enforces
+    along their respective velocity directions, which automatically enforces
     periodic boundary conditions at domain boundaries.
+    
+    Args:
+        f (jax.Array of shape (9, NX, NY)): The distribution functions.
+        
+    Returns:
+        f (jax.Array of shape (9, NX, NY)): The distribution functions after streaming
     """
-
-    f = f.at[1].set(jnp.roll(f[1],  1, axis=0))
-    f = f.at[2].set(jnp.roll(f[2],  1, axis=1))
-    f = f.at[3].set(jnp.roll(f[3], -1, axis=0))
-    f = f.at[4].set(jnp.roll(f[4], -1, axis=1))
-    f = f.at[5].set(jnp.roll(f[5],  1, axis=0))
-    f = f.at[5].set(jnp.roll(f[5],  1, axis=1))
-    f = f.at[6].set(jnp.roll(f[6], -1, axis=0))
-    f = f.at[6].set(jnp.roll(f[6],  1, axis=1))
-    f = f.at[7].set(jnp.roll(f[7], -1, axis=0))
-    f = f.at[7].set(jnp.roll(f[7], -1, axis=1))
-    f = f.at[8].set(jnp.roll(f[8],  1, axis=0))
-    f = f.at[8].set(jnp.roll(f[8], -1, axis=1))
+    
+    f = f.at[RIGHT_DIRS].set(jnp.roll(f[RIGHT_DIRS], 1, axis=1))
+    f = f.at[LEFT_DIRS].set(jnp.roll(f[LEFT_DIRS], -1, axis=1))
+    f = f.at[UP_DIRS].set(jnp.roll(f[UP_DIRS], 1, axis=2))
+    f = f.at[DOWN_DIRS].set(jnp.roll(f[DOWN_DIRS], -1, axis=2))
     return f
+
 
 def get_macroscopic(f, rho, u):
     """Calculate the macroscopic properties (fluid density and velocity)
@@ -76,9 +96,10 @@ def get_macroscopic(f, rho, u):
     """
     
     rho = jnp.sum(f, axis=0)
-    u = u.at[0].set((f[1] + f[5] + f[8] - f[3] - f[6] - f[7]) / rho)
-    u = u.at[1].set((f[2] + f[5] + f[6] - f[4] - f[7] - f[8]) / rho)
+    u = u.at[0].set((jnp.sum(f[RIGHT_DIRS], axis=0) - jnp.sum(f[LEFT_DIRS], axis=0)) / rho)
+    u = u.at[1].set((jnp.sum(f[UP_DIRS], axis=0) - jnp.sum(f[DOWN_DIRS], axis=0)) / rho)
     return rho, u
+
 
 def get_equilibrium(rho, u, feq):
     """Update the equilibrium distribution function based on the macroscopic properties.
@@ -92,20 +113,15 @@ def get_equilibrium(rho, u, feq):
         feq (jax.Array of shape (9, NX, NY)): The equilibrium distribution functions.    
     """
 
-    uxx = u[0] * u[0]
-    uyy = u[1] * u[1]
-    uxy = u[0] * u[1]
-    uu = uxx + uyy
-    feq = feq.at[0].set(4 / 9 * rho * (1 - 1.5 * uu))
-    feq = feq.at[1].set(1 / 9 * rho * (1 - 1.5 * uu + 3 * u[0] + 4.5 * uxx))
-    feq = feq.at[2].set(1 / 9 * rho * (1 - 1.5 * uu + 3 * u[1] + 4.5 * uyy))
-    feq = feq.at[3].set(1 / 9 * rho * (1 - 1.5 * uu - 3 * u[0] + 4.5 * uxx))
-    feq = feq.at[4].set(1 / 9 * rho * (1 - 1.5 * uu - 3 * u[1] + 4.5 * uyy))
-    feq = feq.at[5].set(1 / 36 * rho * (1 + 3 * uu + 3 * (u[0] + u[1]) + 9 * uxy))
-    feq = feq.at[6].set(1 / 36 * rho * (1 + 3 * uu - 3 * (u[0] - u[1]) - 9 * uxy))
-    feq = feq.at[7].set(1 / 36 * rho * (1 + 3 * uu - 3 * (u[0] + u[1]) + 9 * uxy))
-    feq = feq.at[8].set(1 / 36 * rho * (1 + 3 * uu + 3 * (u[0] - u[1]) - 9 * uxy))
+    uc = (u[0, :, :] * VELOCITIES[:, 0, jnp.newaxis, jnp.newaxis] + 
+          u[1, :, :] * VELOCITIES[:, 1, jnp.newaxis, jnp.newaxis])
+    
+    feq = rho[jnp.newaxis, ...] * WEIGHTS[:, jnp.newaxis, jnp.newaxis] * (
+            1 + 3 * uc + 4.5 * uc ** 2 
+            - 1.5 * (u[jnp.newaxis, 0] ** 2 + u[jnp.newaxis, 1] ** 2)
+        )
     return feq
+
 
 def collision(f, feq, omega):
     """Perform the collision step using the single relaxation time (SRT) model. 
@@ -124,27 +140,6 @@ def collision(f, feq, omega):
 
 # --------------------------------- force implementation ---------------------------------
 
-def get_forcing(g, u):
-    """Discretize external force density into lattice forcing term
-    according to Guo Forcing scheme."""
-    
-    gxux = g[0] * u[0]
-    gyuy = g[1] * u[1]
-    gxuy = g[0] * u[1]
-    gyux = g[1] * u[0]
-    
-    forcing = jnp.zeros((9, u.shape[1], u.shape[2]))
-    forcing = forcing.at[0].set(4 / 3 * (- gxux - gyuy))
-    forcing = forcing.at[1].set(1 / 3 * (2 * gxux + g[0] - gyuy))
-    forcing = forcing.at[2].set(1 / 3 * (2 * gyuy + g[1] - gxux))
-    forcing = forcing.at[3].set(1 / 3 * (2 * gxux - g[0] - gyuy))
-    forcing = forcing.at[4].set(1 / 3 * (2 * gyuy - g[1] - gxux))
-    forcing = forcing.at[5].set(1 / 12 * (2 * gxux + 3 * gxuy + g[0] + 3 * gyux + 2 * gyuy + g[1]))
-    forcing = forcing.at[6].set(1 / 12 * (2 * gxux - 3 * gxuy - g[0] - 3 * gyux + 2 * gyuy + g[1]))
-    forcing = forcing.at[7].set(1 / 12 * (2 * gxux + 3 * gxuy - g[0] + 3 * gyux + 2 * gyuy - g[1]))
-    forcing = forcing.at[8].set(1 / 12 * (2 * gxux - 3 * gxuy + g[0] - 3 * gyux + 2 * gyuy - g[1]))
-    
-    return forcing
 
 def get_velocity_correction(g, rho=1):
     """Compute the velocity correction in the presence of external force. 
@@ -162,10 +157,46 @@ def get_velocity_correction(g, rho=1):
 
     return g * 0.5 / rho
 
-def get_source(forcing, omega):
-    """Compute the source term."""
+
+def get_discretized_force(g, u, g_lattice):
+    """Discretize external force density into lattice forcing term using Guo Forcing scheme.
+    
+    Args:
+        g (jax.Array of shape (2, NX, NY)): The external force density.
+        u (jax.Array of shape (2, NX, NY)): The fluid velocity.
+        g_lattice (jax.Array of shape (9, NX, NY)): The discretize external force term.
+    
+    Returns:
+        g_lattice (jax.Array of shape (9, NX, NY)): The discretize external force term.    
+    """
+    
+    uc = (u[0, :, :] * VELOCITIES[:, 0, jnp.newaxis, jnp.newaxis] + 
+          u[1, :, :] * VELOCITIES[:, 1, jnp.newaxis, jnp.newaxis])
+    
+    g_lattice = WEIGHTS[..., jnp.newaxis, jnp.newaxis] * (
+        g[0] * (
+            3 * (VELOCITIES[:, 0, jnp.newaxis, jnp.newaxis] - u[jnp.newaxis, 0,...]) 
+            + 9 * (uc * VELOCITIES[:,0, jnp.newaxis, jnp.newaxis])) 
+        + g[1] * (
+            3 * (VELOCITIES[:, 1, jnp.newaxis, jnp.newaxis] - u[jnp.newaxis, 1,...]) 
+            + 9 * (uc * VELOCITIES[:, 1, jnp.newaxis, jnp.newaxis])))
+    
+    return g_lattice
+
+
+def get_source(g_lattice, omega):
+    """Compute the source term caused by the forcing using Guo Forcing scheme.
+    This term should be added to the distribution functions.
+    
+    Args:
+        g_lattice (jax.Array of shape (9, NX, NY)): The forcing term.
+        omega (scalar): The relaxation parameter (= 1 / relaxation time).
+        
+    Returns:
+        out (jax.Array of shape (9, NX, NY)): The source term.
+    """
    
-    return forcing * (1 - 0.5 * omega)
+    return g_lattice * (1 - 0.5 * omega)
 
 
 # --------------------------------- boundary conditions ---------------------------------
