0b471e48539bb4ea0a9c78aad2f0836a2486e410
haimingz <zhuhaiming921214@gmail.com>
2025-01-25 21:08:44 +0800

    improve multigrid module

--- examples/vortex_induced_vibration_multigrid.py
+++ examples/vortex_induced_vibration_multigrid.py
@@ -39,7 +39,7 @@ TM = 60000             # Total time steps
 
 # multi-block config
 HEIGHT = 10 * D
-WIDTH = [7 * D, 3 * D, 1 * D, 9 * D]
+WIDTHS = [7 * D, 3 * D, 3 * D, 7 * D]
 LEVELS = [-1, 0, -1, -2]
 
 # cylinder position
@@ -49,7 +49,7 @@ Y_OBJ = HEIGHT // 2
 # IB parameters
 N_MARKER = 4 * D
 N_ITER_MDF = 3
-IB_MARGIN = 3          # Margin of the IB region to the cylinder
+IB_MARGIN = 4          # Margin of the IB region to the cylinder
 
 # Physical parameters
 RE = 200               # Reynolds number
@@ -76,7 +76,7 @@ MRT_COL_LEFT4, MRT_SRC_LEFT4 = mrt.precompute_left_matrices(mg.get_omega(NU, LEV
 
 # ====================== IBM parameters ==================
 
-X_OBJ_LOCAL = X_OBJ - WIDTH[0]
+X_OBJ_LOCAL = X_OBJ - WIDTHS[0]
 Y_OBJ_LOCAL = Y_OBJ
 
 THETA_MAKERS = jnp.linspace(0, jnp.pi * 2, N_MARKER, dtype=jnp.float32, endpoint=False)
@@ -90,25 +90,25 @@ IB_START_X = int(X_OBJ_LOCAL - 0.5 * D - IB_MARGIN)
 IB_START_Y = int(Y_OBJ_LOCAL - 0.5 * D - IB_MARGIN)
 IB_SIZE = D + IB_MARGIN * 2
  
-X, Y = jnp.meshgrid(jnp.arange(WIDTH[1]), jnp.arange(HEIGHT), indexing='ij')
+X, Y = jnp.meshgrid(jnp.arange(WIDTHS[1]), jnp.arange(HEIGHT), indexing='ij')
 
 
 # ======================= define variables =====================
 
-f1, feq1, rho1, u1 = mg.generate_block_data(WIDTH[0], HEIGHT, LEVELS[0])
-f2, feq2, rho2, u2 = mg.generate_block_data(WIDTH[1], HEIGHT, LEVELS[1])
-f3, feq3, rho3, u3 = mg.generate_block_data(WIDTH[2], HEIGHT, LEVELS[2])
-f4, feq4, rho4, u4 = mg.generate_block_data(WIDTH[3], HEIGHT, LEVELS[3])
+f1, feq1, rho1, u1 = mg.generate_block_data(WIDTHS[0], HEIGHT, LEVELS[0])
+f2, feq2, rho2, u2 = mg.generate_block_data(WIDTHS[1], HEIGHT, LEVELS[1])
+f3, feq3, rho3, u3 = mg.generate_block_data(WIDTHS[2], HEIGHT, LEVELS[2])
+f4, feq4, rho4, u4 = mg.generate_block_data(WIDTHS[3], HEIGHT, LEVELS[3])
 
 u1 = u1.at[0].set(U0)
 u2 = u2.at[0].set(U0)
 u3 = u3.at[0].set(U0)
 u4 = u4.at[0].set(U0)
 
-f1 = lbm.get_equilibrium(rho1, u1, f1)
-f2 = lbm.get_equilibrium(rho2, u2, f2)
-f3 = lbm.get_equilibrium(rho3, u3, f3)
-f4 = lbm.get_equilibrium(rho4, u4, f4)
+f1 = f1.at[:,1:-1,1:-1].set(lbm.get_equilibrium(rho1, u1, f1[:,1:-1,1:-1]))
+f2 = f2.at[:,1:-1,1:-1].set(lbm.get_equilibrium(rho2, u2, f2[:,1:-1,1:-1]))
+f3 = f3.at[:,1:-1,1:-1].set(lbm.get_equilibrium(rho3, u3, f3[:,1:-1,1:-1]))
+f4 = f4.at[:,1:-1,1:-1].set(lbm.get_equilibrium(rho4, u4, f4[:,1:-1,1:-1]))
 
 d = jnp.zeros((2), dtype=jnp.float32) 
 v = jnp.zeros((2), dtype=jnp.float32) 
@@ -117,15 +117,15 @@ h = jnp.zeros((2), dtype=jnp.float32)
 
 v = v.at[1].set(1e-3) 
 
-feq0 = f4[:,0,0]
+feq_init = f1[:,1,1]
 
 # ======================= compute routine =====================
 
 
 def macro_collision(f, feq, rho, u, left_matrix):  
-    rho, u = lbm.get_macroscopic(f, rho, u)
+    rho, u = lbm.get_macroscopic(f[:, 1:-1, 1:-1], rho, u)
     feq = lbm.get_equilibrium(rho, u, feq)
-    f = mrt.collision(f, feq, left_matrix)
+    f = f.at[:, 1:-1, 1:-1].set(mrt.collision(f[:, 1:-1, 1:-1], feq, left_matrix))
     return f, feq, rho, u
 
 def solve_fsi(f, rho, u, d, v, a, h):
@@ -141,7 +141,7 @@ def solve_fsi(f, rho, u, d, v, a, h):
     u_slice = jax.lax.dynamic_slice(u, (0, ib_start_x, ib_start_y), (2, IB_SIZE, IB_SIZE))
     X_slice = jax.lax.dynamic_slice(X, (ib_start_x, ib_start_y), (IB_SIZE, IB_SIZE))
     Y_slice = jax.lax.dynamic_slice(Y, (ib_start_x, ib_start_y), (IB_SIZE, IB_SIZE))
-    f_slice = jax.lax.dynamic_slice(f, (0, ib_start_x, ib_start_y), (9, IB_SIZE, IB_SIZE))
+    f_slice = jax.lax.dynamic_slice(f, (0, ib_start_x + 1, ib_start_y + 1), (9, IB_SIZE, IB_SIZE))
     
     # calculate ibm force
     g_lattice, h_markers = ib.multi_direct_forcing(u_slice, X_slice, Y_slice, 
@@ -150,7 +150,7 @@ def solve_fsi(f, rho, u, d, v, a, h):
 
     # apply the force to the lattice
     s_slice = mrt.get_source(g_lattice, MRT_SRC_LEFT2)    
-    f = jax.lax.dynamic_update_slice(f, f_slice + s_slice, (0, ib_start_x, ib_start_y))
+    f = jax.lax.dynamic_update_slice(f, f_slice + s_slice, (0, ib_start_x + 1, ib_start_y + 1))
 
     # apply the force to the cylinder
     h = ib.get_force_to_obj(h_markers)
@@ -159,50 +159,53 @@ def solve_fsi(f, rho, u, d, v, a, h):
     
     return f, d, v, a, h
 
-def update_coarse0(f1, f2, feq2, rho2, u2, f3, d, v, a, h):
+def update_coarse0(f2, feq2, rho2, u2, d, v, a, h):
     
     f2, feq2, rho2, u2 = macro_collision(f2, feq2, rho2, u2, MRT_COL_LEFT2)
     f2, d, v, a, h = solve_fsi(f2, rho2, u2, d, v, a, h)
     
-    f1 = mg.accumulate(f2, f1, dir='left')
-    f3 = mg.accumulate(f2, f3, dir='right')
-    f2 = lbm.streaming(f2)
-    f2, f1 = mg.explosion(f2, f1, dir='right')
-    f2, f3 = mg.explosion(f2, f3, dir='left')
+    f2 = mg.accumulate(f2, dir='left')
+    f2 = mg.accumulate(f2, dir='right')
+    f2 = mg.streaming(f2)
+    f2 = mg.propagate(f2, dir='right')
+    f2 = mg.propagate(f2, dir='left')
     
-    return f1, f2, feq2, rho2, u2, f3, d, v, a, h
+    return f2, feq2, rho2, u2, d, v, a, h
 
 def update_coarse1(f1, feq1, rho1, u1, 
                    f2, feq2, rho2, u2,
                    f3, feq3, rho3, u3, 
-                   f4, d, v, a, h):
+                   d, v, a, h):
     
     # collision (mesh1 & mesh3)
     f1, feq1, rho1, u1 = macro_collision(f1, feq1, rho1, u1, MRT_COL_LEFT1)
     f3, feq3, rho3, u3 = macro_collision(f3, feq3, rho3, u3, MRT_COL_LEFT3)
     
     # reset ghost cells (mesh1 & mesh3)
-    f1 = mg.clear_ghost(f1, location='right')
-    f3 = mg.clear_ghost(f3, location='left')
+    f2 = mg.clear_ghost(f2, location='left')
+    f2 = mg.clear_ghost(f2, location='right')
+    
+    f2 = mg.explosion(f1, f2, dir='right')
+    f2 = mg.explosion(f3, f2, dir='left')
     
     # update fine mesh twice (mesh2)
-    f1, f2, feq2, rho2, u2, f3, d, v, a, h = update_coarse0(f1, f2, feq2, rho2, u2, f3, d, v, a, h)
-    f1, f2, feq2, rho2, u2, f3, d, v, a, h = update_coarse0(f1, f2, feq2, rho2, u2, f3, d, v, a, h)
+    f2, feq2, rho2, u2, d, v, a, h = update_coarse0(f2, feq2, rho2, u2, d, v, a, h)
+    f2, feq2, rho2, u2, d, v, a, h = update_coarse0(f2, feq2, rho2, u2, d, v, a, h)
     
     # streaming (mesh1 & mesh3)
-    f1 = f1.at[:,:-1].set(lbm.streaming(f1[:,:-1]))
-    f1 = mg.coalescence(f1, dir='left')
-    f1 = lbm.velocity_boundary(f1, U0, 0, loc='left')
+    f1 = mg.streaming(f1)
+    f1 = mg.coalescence(f2, f1, dir='left')
+    f1 = f1.at[:, 1:-1, 1:-1].set(lbm.velocity_boundary(f1[:,1:-1, 1:-1], U0, 0, loc='left'))
      
-    f4 = mg.accumulate(f3, f4, dir='right')
-    f3 = f3.at[:, 1:].set(lbm.streaming(f3[:,1:]))
-    f3 = mg.coalescence(f3, dir='right')
-    f3, f4 = mg.explosion(f3, f4, dir='left')
+    f3 = mg.accumulate(f3, dir='right')
+    f3 = mg.streaming(f3)
+    f3 = mg.propagate(f3, dir='left')
+    f3 = mg.coalescence(f2, f3, dir='right')
         
     return (f1, feq1, rho1, u1, 
             f2, feq2, rho2, u2,
             f3, feq3, rho3, u3, 
-            f4, d, v, a, h,)
+            d, v, a, h,)
 
 @jax.jit
 def update_coarse2(f1, feq1, rho1, u1, 
@@ -215,28 +218,30 @@ def update_coarse2(f1, feq1, rho1, u1,
     f4, feq4, rho4, u4 = macro_collision(f4, feq4, rho4, u4, MRT_COL_LEFT4)
     
     # reset ghost cells of mesh4
-    f4 = mg.clear_ghost(f4, location='left')
+    f3 = mg.clear_ghost(f3, location='right')   
+     
+    f3 = mg.explosion(f4, f3, dir='left')
     
     # update fine mesh twice (mesh1 & mesh2 & mesh3)
     (f1, feq1, rho1, u1, 
      f2, feq2, rho2, u2,
      f3, feq3, rho3, u3, 
-     f4, d, v, a, h) = update_coarse1(f1, feq1, rho1, u1, 
-                                       f2, feq2, rho2, u2,
-                                       f3, feq3, rho3, u3, 
-                                       f4, d, v, a, h)    
+     d, v, a, h) = update_coarse1(f1, feq1, rho1, u1, 
+                                  f2, feq2, rho2, u2,
+                                  f3, feq3, rho3, u3, 
+                                  d, v, a, h)    
     (f1, feq1, rho1, u1, 
      f2, feq2, rho2, u2,
      f3, feq3, rho3, u3, 
-     f4, d, v, a, h) = update_coarse1(f1, feq1, rho1, u1, 
-                                       f2, feq2, rho2, u2,
-                                       f3, feq3, rho3, u3, 
-                                       f4, d, v, a, h)   
+     d, v, a, h) = update_coarse1(f1, feq1, rho1, u1, 
+                                  f2, feq2, rho2, u2,
+                                  f3, feq3, rho3, u3, 
+                                  d, v, a, h)   
 
     # streaming (mesh4)
-    f4 = f4.at[:,1:].set(lbm.streaming(f4[:,1:]))
-    f4 = mg.coalescence(f4, dir='right')   
-    f4 = lbm.boundary_equilibrium(f4, feq0[:, jnp.newaxis], loc='right')
+    f4 = mg.streaming(f4)
+    f4 = mg.coalescence(f3, f4, dir='right')   
+    f4 = f4.at[:, 1:-1, 1:-1].set(lbm.boundary_equilibrium(f4[:,1:-1,1:-1], feq_init[:, jnp.newaxis], loc='right'))
     
     return (f1, feq1, rho1, u1, 
             f2, feq2, rho2, u2, 
@@ -260,26 +265,26 @@ if PLOT == 'curl':
     )
 
     im1 = plt.imshow(
-        post.calculate_curl(u1[:,:-1]).T, 
-        extent=[0, WIDTH[0] / D, 0, HEIGHT / D],
+        post.calculate_curl(u1).T, 
+        extent=[0, WIDTHS[0] / D, 0, HEIGHT / D],
         **kwargs
     )
     
     im2 = plt.imshow(
         post.calculate_curl(u2).T * 2, 
-        extent=[WIDTH[0] / D, (WIDTH[0] + WIDTH[1]) / D, 0, HEIGHT / D],
+        extent=[WIDTHS[0] / D, (WIDTHS[0] + WIDTHS[1]) / D, 0, HEIGHT / D],
         **kwargs
     )
     
     im3 = plt.imshow(
-        post.calculate_curl(u3[:,1:]).T, 
-        extent=[(WIDTH[0] + WIDTH[1]) / D, (WIDTH[0] + WIDTH[1] + WIDTH[2]) / D, 0, HEIGHT / D],
+        post.calculate_curl(u3).T, 
+        extent=[(WIDTHS[0] + WIDTHS[1]) / D, (WIDTHS[0] + WIDTHS[1] + WIDTHS[2]) / D, 0, HEIGHT / D],
         **kwargs
     )
     
     im4 = plt.imshow(
-        post.calculate_curl(u4[:,1:]).T,
-        extent=[ (WIDTH[0] + WIDTH[1] + WIDTH[2]) / D,  (WIDTH[0] + WIDTH[1] + WIDTH[2] + WIDTH[3]) / D, 0, HEIGHT / D],
+        post.calculate_curl(u4).T,
+        extent=[ (WIDTHS[0] + WIDTHS[1] + WIDTHS[2]) / D,  (WIDTHS[0] + WIDTHS[1] + WIDTHS[2] + WIDTHS[3]) / D, 0, HEIGHT / D],
         **kwargs
     )
 
@@ -294,9 +299,9 @@ if PLOT == 'curl':
     plt.gca().add_artist(circle)
     
     # draw the boundaries of mesh blocks
-    plt.axvline(WIDTH[0] / D, color="g", linestyle="--", linewidth=0.5)
-    plt.axvline((WIDTH[0] + WIDTH[1]) / D, color="g", linestyle="--", linewidth=0.5)   
-    plt.axvline((WIDTH[0] + WIDTH[1] + WIDTH[2]) / D, color="g", linestyle="--", linewidth=0.5)
+    plt.axvline(WIDTHS[0] / D, color="g", linestyle="--", linewidth=0.5)
+    plt.axvline((WIDTHS[0] + WIDTHS[1]) / D, color="g", linestyle="--", linewidth=0.5)   
+    plt.axvline((WIDTHS[0] + WIDTHS[1] + WIDTHS[2]) / D, color="g", linestyle="--", linewidth=0.5)
 
     # mark the initial position of the cylinder
     plt.plot(X_OBJ / D, Y_OBJ / D, 
@@ -334,10 +339,10 @@ for t in tqdm(range(TM)):
     if t % PLOT_EVERY == 0 and t > PLOT_AFTER:
     
         if PLOT == 'curl':
-            im1.set_data(post.calculate_curl(u1[:,:-1]).T * 2)
+            im1.set_data(post.calculate_curl(u1).T * 2)
             im2.set_data(post.calculate_curl(u2).T * 4)
-            im3.set_data(post.calculate_curl(u3[:,1:]).T * 2)
-            im4.set_data(post.calculate_curl(u4[:,1:]).T)
+            im3.set_data(post.calculate_curl(u3).T * 2)
+            im4.set_data(post.calculate_curl(u4).T)
             circle.center = ((X_OBJ + d[0]) / D, (Y_OBJ + d[1]) / D)
             plt.pause(0.01)
 
--- vivsim/multigrid.py
+++ vivsim/multigrid.py
@@ -1,10 +1,48 @@
 """
-This module contains functions for transporting particles across subdomains with 
-different resolutions.
+This module contains functions for streaming particles across blocks with different resolutions.
+
+Assumptions:
+    - All blocks are rectangles. 
+    - The outermost columns/rows of f are ghost layers to store temporary data.
+    - The second outermost columns/rows of f are the real boundary of the block. 
+    - The grid refinement ratio between two neighboring blocks is 2. 
+
+Visualization of a block showing the ghost layers (G) and real boundaries (B).
+
+    ---------------------------------   
+    |   | G | G | G | G | G | G |   |
+    ---------------------------------
+    | G | B | B | B | B | B | B | G |
+    ---------------------------------
+    | G | B |   |   |   |   | B | G |
+    ---------------------------------
+    | G | B |   |   |   |   | B | G |
+    ---------------------------------
+    | G | B | B | B | B | B | B | G |
+    ---------------------------------
+    |   | G | G | G | G | G | G |   |
+    --------------------------------- 
+
+Coarse-to-fine transportation:
+    The process is explosion (BS) -> propagate (AS) -> propagate (AS)
+    - In the explosion step, the particles are transferred 
+        from the coarse block to the ghost layer.
+    - In the propagate step, the particles are transferred 
+        from the ghost layer to the fine block.
+
+Fine-to-coarse transportation:
+    The process is accumulation (BS) -> accumulation (BS) -> coalescence (AS)
+    - In the accumulation process, the particles are transferred
+        from the fine block to the ghost layer.
+    - In the coalescence step, the the particles are transferred
+        from the ghost layer to the coarse block.
+        
+where BS stands for before streaming and AS stands for after streaming.
 """
 
 import jax
 import jax.numpy as jnp
+from . import lbm
 
 # lattice directions
 RIGHT_DIRS = jnp.array([1, 5, 8])
@@ -13,115 +51,159 @@ UP_DIRS = jnp.array([2, 5, 6])
 DOWN_DIRS = jnp.array([4, 7, 8])
 
 
-def explosion(f_fine: jax.Array, f_coarse: jax.Array, dir: str)-> tuple[jax.Array, jax.Array]:
+def explosion(f_coarse: jax.Array, f_fine: jax.Array, dir: str)-> jax.Array:
     """
-    Transfer particles from the coarse grid to the fine grid. Note that the outermost layer
-    of the coarse grid is used as a 'ghost' layer to store information from the fine grid.
-    Therefore particles from the second outermost layer of the coarse grid is transferred
-    to the fine grid.
+    Transfer particles from the coarse block to the ghost layer. 
+    
+    This is the first step of coarse-to-fine transportation. It should be carried out
+    BEFORE the streaming of the coarse block since it deals with the outgoing particles.
     
     Args:
-        f_fine (jax.Array): The distribution function on the fine grid.
-        f_coarse (jax.Array): The distribution function on the coarse grid.
-        dir (str): The direction of the explosion. It can be 'left', 'right', 'up', or 'down', 
-                   For example, if dir='left', the coarse grid should be on the right, 
-                   transporting particles to the fine grid on the left. 
+        f_coarse (jax.Array): The distribution function of the coarse block.
+        f_fine (jax.Array): The distribution function of the fine block.
+        dir (str): The direction of the transportation, can be 'left', 'right', 'up', or 'down', 
+                   For example, if dir='left', the particles are transporting from  
+                   the block on the right to the block on the left.
     
     Returns:
-        f_fine (jax.Array): The updated distribution functions on the fine grid.
-        f_coarse (jax.Array): The updated distribution functions on the coarse grid.
+        f_fine (jax.Array): The updated distribution functions of the fine block.
     """
     
     if dir == 'left':
-        f_fine = f_fine.at[LEFT_DIRS, -1, 0::2].set(f_coarse[LEFT_DIRS, 1])
-        f_fine = f_fine.at[LEFT_DIRS, -1, 1::2].set(f_coarse[LEFT_DIRS, 1])
+        f_fine = f_fine.at[LEFT_DIRS, -1, 1:-1:2].set(f_coarse[LEFT_DIRS, 1, 1:-1])
+        f_fine = f_fine.at[LEFT_DIRS, -1, 2:-1:2].set(f_coarse[LEFT_DIRS, 1, 1:-1])
     elif dir == 'right':
-        f_fine = f_fine.at[RIGHT_DIRS, 0, 0::2].set(f_coarse[RIGHT_DIRS, -2])
-        f_fine = f_fine.at[RIGHT_DIRS, 0, 1::2].set(f_coarse[RIGHT_DIRS, -2])
+        f_fine = f_fine.at[RIGHT_DIRS, 0, 1:-1:2].set(f_coarse[RIGHT_DIRS, -2, 1:-1])
+        f_fine = f_fine.at[RIGHT_DIRS, 0, 2:-1:2].set(f_coarse[RIGHT_DIRS, -2, 1:-1])
     elif dir == 'up':
-        f_fine = f_fine.at[UP_DIRS, 0::2, 0].set(f_coarse[UP_DIRS, :, -2])
-        f_fine = f_fine.at[UP_DIRS, 1::2, 0].set(f_coarse[UP_DIRS, :, -2])
+        f_fine = f_fine.at[UP_DIRS, 1:-1:2, 0].set(f_coarse[UP_DIRS, 1:-1, -2])
+        f_fine = f_fine.at[UP_DIRS, 2:-1:2, 0].set(f_coarse[UP_DIRS, 1:-1, -2])
     elif dir == 'down':
-        f_fine = f_fine.at[DOWN_DIRS, 0::2, -1].set(f_coarse[DOWN_DIRS, :, 1])
-        f_fine = f_fine.at[DOWN_DIRS, 1::2, -1].set(f_coarse[DOWN_DIRS, :, 1]) 
-    return f_fine, f_coarse
+        f_fine = f_fine.at[DOWN_DIRS, 1:-1:2, -1].set(f_coarse[DOWN_DIRS, 1:-1, 1])
+        f_fine = f_fine.at[DOWN_DIRS, 2:-1:2, -1].set(f_coarse[DOWN_DIRS, 1:-1, 1]) 
+    return f_fine
 
-def accumulate(f_fine: jax.Array, f_coarse: jax.Array, dir: str)-> tuple[jax.Array, jax.Array]:
+def propagate(f: jax.Array, dir: str)-> jax.Array:
     """
-    This is the first step of transferring particles from the fine grid to the coarse grid. 
-    In this step, particles from the fine grid are accumulated in the ghost layer of the coarse grid,
-    which is the outermost layer of the coarse grid.
+    Transfer particles from the ghost layer into the block. 
+    
+    This is the second step of coarse-to-fine transportation. It should be carried out
+    AFTER the streaming since it deals with the incoming particles.
+    
     
     Args:
-        f_fine (jax.Array): The distribution function on the fine grid.
-        f_coarse (jax.Array): The distribution function on the coarse grid.
-        dir (str): The direction of the accumulation. It can be 'left', 'right', 'up', or 'down', 
-                   For example, if dir='left', the fine grid should be on the left,
-                   transporting particles to the coarse grid on the left.
+        f (jax.Array): The distribution function of the grid.
+        dir (str): The direction of the transportation, can be 'left', 'right', 'up', or 'down', 
+                   For example, if dir='left', the particles are transporting from  
+                   the block on the right to the block on the left.
+    
+    Returns:
+        f (jax.Array): The updated distribution functions of the grid.
+    """
+    
+    if dir == 'left':
+        f = f.at[6,-2,1:-1].set(jnp.roll(f[6,-1,1:-1],1,axis=0))
+        f = f.at[7,-2,1:-1].set(jnp.roll(f[7,-1,1:-1],-1,axis=0))
+        f = f.at[3,-2,1:-1].set(f[3,-1,1:-1])
+    elif dir == 'right':
+        f = f.at[5,1,1:-1].set(jnp.roll(f[5,0,1:-1],1,axis=0))
+        f = f.at[8,1,1:-1].set(jnp.roll(f[8,0,1:-1],-1,axis=0))
+        f = f.at[1,1,1:-1].set(f[1,0,1:-1])
+    elif dir == 'up':
+        f = f.at[5,1:-1,1].set(jnp.roll(f[5,1:-1,0],1,axis=0))
+        f = f.at[6,1:-1,1].set(jnp.roll(f[6,1:-1,0],-1,axis=0))
+        f = f.at[2,1:-1,1].set(f[2,1:-1,0])
+    elif dir == 'down':
+        f = f.at[8,1:-1,-2].set(jnp.roll(f[8,1:-1,-1],1,axis=0))
+        f = f.at[7,1:-1,-2].set(jnp.roll(f[7,1:-1,-1],-1,axis=0))
+        f = f.at[4,1:-1,-2].set(f[4,1:-1,-1])
+    return f
+
+def accumulate(f: jax.Array, dir: str)-> jax.Array:
+    """
+    Transfer particles from the fine block to the ghost layer.
+    
+    This is the first step of fine-to-coarse transportation. It should be carried out
+    BEFORE the streaming of the fine block since it deals with the outgoing particles.
+    
+    Args:
+        f (jax.Array): The distribution function of the fine block.
+        dir (str): The direction of the transportation, can be 'left', 'right', 'up', or 'down', 
+                   For example, if dir='left', the particles are transporting from  
+                   the block on the right to the block on the left.
                    
     Returns:
-        f_fine (jax.Array): The updated distribution functions on the fine grid.
-        f_coarse (jax.Array): The updated distribution functions on the coarse grid.
+        f (jax.Array): The updated distribution function of the fine block.
     """
     
     if dir == 'left':
-        return f_coarse.at[LEFT_DIRS, -1].add(f_fine[LEFT_DIRS, 0, 0::2] + f_fine[LEFT_DIRS, 0, 1::2])
+        f = f.at[6, 0, 1:-1].add(jnp.roll(f[6, 1, 1:-1], 1, axis=0))
+        f = f.at[7, 0, 1:-1].add(jnp.roll(f[7, 1, 1:-1], -1, axis=0))
+        f = f.at[3, 0, 1:-1].add(f[3, 1, 1:-1])
     elif dir == 'right':
-        return f_coarse.at[RIGHT_DIRS, 0].add(f_fine[RIGHT_DIRS,-1, 0::2] + f_fine[RIGHT_DIRS,-1, 1::2])
+        f = f.at[5, -1, 1:-1].add(jnp.roll(f[5, -2, 1:-1], 1, axis=0))
+        f = f.at[8, -1, 1:-1].add(jnp.roll(f[8, -2, 1:-1], -1, axis=0))
+        f = f.at[1, -1, 1:-1].add(f[1, -2, 1:-1])
     elif dir == 'up':
-        return f_coarse.at[UP_DIRS, :, 0].add(f_fine[UP_DIRS, 0::2, -1] + f_fine[UP_DIRS, 1::2, -1])
+        f = f.at[5, 1:-1, 0].add(jnp.roll(f[5, 1:-1, 1], 1, axis=0))
+        f = f.at[6, 1:-1, 0].add(jnp.roll(f[6, 1:-1, 1], -1, axis=0))
+        f = f.at[2, 1:-1, 0].add(f[2, 1:-1, 1])
     elif dir == 'down':
-        return f_coarse.at[DOWN_DIRS, :, -1].add(f_fine[DOWN_DIRS, 0::2, 0] + f_fine[DOWN_DIRS, 1::2, 0])
+        f = f.at[8, 1:-1, -1].add(jnp.roll(f[8, 1:-1, -2], 1, axis=0))
+        f = f.at[7, 1:-1, -1].add(jnp.roll(f[7, 1:-1, -2], -1, axis=0))
+        f = f.at[4, 1:-1, -1].add(f[4, 1:-1, -2])
+    return f 
 
-
-def coalescence(f_coarse: jax.Array, dir:str)-> jax.Array:
+def coalescence(f_fine: jax.Array, f_coarse: jax.Array, dir:str)-> jax.Array:
     """
-    This is the second step of transferring particles from the fine grid to the coarse grid.
-    In this step, particles accumulated in the ghost layer of the coarse grid are coalesced
-    into the real coarse grid. 
+    Transfer particles from the ghost layer to the coarse block.
+    
+    This is the second step of fine-to-coarse transportation. It should be carried out
+    AFTER the streaming of the coarse block since it deals with the incoming particles.
     
     Args:
-        f_coarse (jax.Array): The distribution function on the coarse grid.
-        dir (str): The direction of the coalescence. It can be 'left', 'right', 'up', or 'down', 
-                   For example, if dir='left', the fine grid should be on the left,
-                   transporting particles to the coarse grid on the left.
+        f_fine (jax.Array): The distribution function of the fine block.
+        f_coarse (jax.Array): The distribution function of the coarse block.
+        dir (str): The direction of the transportation, can be 'left', 'right', 'up', or 'down', 
+                   For example, if dir='left', the particles are transporting from  
+                   the block on the right to the block on the left.
                    
     Returns:
-        f_coarse (jax.Array): The updated distribution functions on the coarse grid.
+        f_coarse (jax.Array): The updated distribution function of the coarse block.
     """
     
     if dir == 'left':
-        return f_coarse.at[LEFT_DIRS, -2].set(f_coarse[LEFT_DIRS, -1]/4)
+        return f_coarse.at[LEFT_DIRS, -2, 1:-1].set((f_fine[LEFT_DIRS, 0, 1:-1:2] + f_fine[LEFT_DIRS, 0, 2:-1:2]) / 4)
     elif dir == 'right':
-        return f_coarse.at[RIGHT_DIRS, 1].set(f_coarse[RIGHT_DIRS, 0]/4)
+        return f_coarse.at[RIGHT_DIRS, 1, 1:-1].set((f_fine[RIGHT_DIRS, -1, 1:-1:2] + f_fine[RIGHT_DIRS, -1, 2:-1:2]) / 4)
     elif dir == 'up':
-        return f_coarse.at[UP_DIRS, :, -2].set(f_coarse[UP_DIRS, :, -1]/4)
+        return f_coarse.at[UP_DIRS, 1:-1, 1].set((f_fine[UP_DIRS, 1:-1:2, -1] + f_fine[UP_DIRS, 2:-1:2, -1]) / 4)
     elif dir == 'down':
-        return f_coarse.at[DOWN_DIRS, :, 1].set(f_coarse[DOWN_DIRS, :, 0]/4)
+        return f_coarse.at[DOWN_DIRS, 1:-1, -2].set((f_fine[DOWN_DIRS, 1:-1:2, 0] + f_fine[DOWN_DIRS, 2:-1:2, 0]) / 4)
 
 
-def clear_ghost(f_coarse: jax.Array, location:str)-> jax.Array:
+def clear_ghost(f: jax.Array, location:str)-> jax.Array:
     """
-    Clear the ghost layer of the coarse grid.
+    Clear the ghost layer of the coarse block.
     
     Args:
-        f_coarse (jax.Array): The distribution function on the coarse grid.
-        location (str): The location of the ghost layer. It can be 'left', 'right', 'top', or 'bottom'.
+        f_fine (jax.Array): The distribution function of the coarse block.
+        location (str): The location of the ghost layer. 
+            It can be 'left', 'right', 'top', or 'bottom'.
         
     Returns:
-        f_coarse (jax.Array): The updated distribution functions on the coarse grid.
+        f_fine (jax.Array): The updated distribution function of the coarse block.
     """
     
-    if location == 'left':
-        return f_coarse.at[:, 0].set(0)
-    elif location == 'right':
-        return f_coarse.at[:, -1].set(0)
-    elif location == 'top':
-        return f_coarse.at[:, :, 0].set(0)
-    elif location == 'bottom':
-        return f_coarse.at[:, :, -1].set(0)
-    
-    
+    if location == 'left' or location == 'all':
+        return f.at[:, 0].set(0)
+    elif location == 'right' or location == 'all':
+        return f.at[:, -1].set(0)
+    elif location == 'top' or location == 'all':
+        return f.at[:, :, -1].set(0)
+    elif location == 'bottom' or location == 'all':
+        return f.at[:, :, 0].set(0)
+     
 def get_omega(nu, level=0):
     """
     Compute the relaxation parameter omega for different levels of refinement.
@@ -138,27 +220,32 @@ def get_omega(nu, level=0):
     
     return omega
 
-
 def generate_block_data(width, height, level=0):
     """
     Generate the fluid variables for a block of fluid on a grid with a given level of refinement.
+    
+    Note that the size of f is two column/row larger than the size of other variables because
+    the outermost column/row of f is reserved for the ghost layer.
     
     Args:
         width (int): The width of the block (in lattice unit).
         height (int): The height of the block (in lattice unit).
         level (int): The level of refinement. 
-            Default is 0. 
+            Default is 0, can be negative number. 
             Greater level means finer grid.
-            Negative level means coarser grid.
+            Smaller level means coarser grid.
     """
   
     nx = int(width * 2 ** level)
     ny = int(height * 2 ** level)
     
     # fluid variables
-    f = jnp.zeros((9, nx, ny), dtype=jnp.float32)
+    f = jnp.zeros((9, nx + 2, ny + 2), dtype=jnp.float32)
     feq = jnp.zeros((9, nx, ny), dtype=jnp.float32) 
     rho = jnp.ones((nx, ny), dtype=jnp.float32) 
     u = jnp.zeros((2, nx, ny), dtype=jnp.float32)
     
     return f, feq, rho, u
+
+def streaming(f):
+    return f.at[:, 1:-1, 1:-1].set(lbm.streaming(f[:, 1:-1, 1:-1]))
